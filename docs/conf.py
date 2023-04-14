be4a2126b8ce625b5283d46fecbd8a676749f982
Lasse Lambrechts <lasse.lambrechts@bt.no>
2023-04-14 15:40:48 +0200

    read the docs
    Signed-off-by: Lasse Lambrechts <lasse.lambrechts@bt.no>

---
+++ docs/conf.py
@@ -0,0 +1,53 @@
+"""Configuration file for the Sphinx documentation builder."""
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+import airflow_dbt_python
+
+# -- Project information -----------------------------------------------------
+
+project = airflow_dbt_python.__title__
+copyright = airflow_dbt_python.__copyright__
+author = airflow_dbt_python.__author__
+
+# The full version, including alpha/beta/rc tags
+release = version = airflow_dbt_python.__version__
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+autodoc_typehints = "description"
+
+html_css_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/hack-font/3.3.0/web/hack.min.css"
+]
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. orion-nais documentation master file, created by
+   sphinx-quickstart on Fri Apr 14 14:39:27 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to orion-nais's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
